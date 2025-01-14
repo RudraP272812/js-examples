55a17e3641995a31383f4e8e65f024ed90fa79a3
Rudra <r_patel164264@fanshaweonline.ca>
2025-01-14 17:29:01 -0500

    stable working commit

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from "express";
-const PORT = 3000;
+const PORT = 5000;
 const app = express();
-app.use("/", express.static("module-examples/basic-modules"));
+// //i used the req and res as i already used this before and ysed it for default route.
+// app.get("/", (req, res) => {
+//   res.send("Hello, Welcome to my server!");
+// });
+
+// route to my name lab2
+app.use("/_rudra_patel_lab2", express.static("module-examples/basic-modules"));
+
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
